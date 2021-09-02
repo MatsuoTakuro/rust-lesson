07725920119dcd1eb756a97c7916826b89d91252
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-09-02 17:21:42 +0900

    Learned about 17. traits

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 // mod generics;
 // mod lifetime;
 // mod structs;
-mod enums;
+// mod enums;
+mod traits;
 
 
 fn main() {
@@ -15,7 +16,8 @@ fn main() {
     // generics::run();
     // lifetime::run();
     // structs::run();
-    enums::run();
+    // enums::run();
+    traits::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
---
+++ src/traits.rs
@@ -0,0 +1,91 @@
+use std::{panic::Location, thread::LocalKey};
+
+trait Fruits {
+    fn price(&self) -> u32;
+}
+struct Apple;
+impl Fruits for Apple {
+    fn price(&self) -> u32 {
+        10
+    }
+}
+struct Banana;
+impl Fruits for Banana {
+    fn price(&self) -> u32 {
+        5
+    }
+}
+trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+trait  Message {
+    fn message(&self) -> String {
+        String::from("Message")
+    }
+}
+
+struct  NewArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+impl Summary for NewArticle {
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
+}
+impl Message for NewArticle {}
+
+struct  Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+pub fn run() {
+    let apple = Apple {};
+    let banana = Banana {};
+    get_price(apple);
+    get_price(banana);
+    let tweet = Tweet {
+        username: String::from("horse_ebook"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+    let article = NewArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "the Pittsburgh Penguins once again are the best \
+            hockey team in the NHL."),
+    };
+    println!("{}", article.summarize());
+    notify(&tweet);
+    notify(&article);
+    notify_another(&article);
+    // notify_another(&tweet);
+}
+fn get_price<T: Fruits>(fruits: T) {
+    println!("price is : {}", fruits.price());
+}
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn notify_another(item: &(impl Summary + Message)) {
+    println!("Breaking news! {}", item.summarize());
+    println!("Message! {}", item.message());
+}
