0db97d963cb00245d55ada5f05c4f3417f1b5315
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-09-06 23:21:24 +0900

    Learned about 21. Debug tool

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-// mod vars;
-// mod stack_heap;
-// mod ownership;
-// mod generics;
-// mod lifetime;
-// mod structs;
-// mod enums;
-// mod traits;
+extern crate lib_demo;
+mod debug;
+mod vars;
+mod stack_heap;
+mod ownership;
+mod generics;
+mod lifetime;
+mod structs;
+mod enums;
+mod traits;
 mod error_handling;
 mod unit_test;
 
-
 fn main() {
-    // println!("Hello, world!");
-    // vars::run();
-    // stack_heap::run();
-    // ownership::run();
-    // generics::run();
-    // lifetime::run();
-    // structs::run();
-    // enums::run();
-    // traits::run();
+    println!("Hello, world!");
+    debug::run();
+    vars::run();
+    stack_heap::run();
+    ownership::run();
+    generics::run();
+    lifetime::run();
+    structs::run();
+    enums::run();
+    traits::run();
     error_handling::run();
-    // vars::sub_a::func_a();
-    // vars::sub_b::func_b();
+    vars::sub_a::func_a();
+    vars::sub_b::func_b();
+    lib_demo::print_random_number();
 }
